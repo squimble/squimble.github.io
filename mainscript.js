05a2ec49cdefe63fcb14c0951bf7050bb0dd6bee
squimble <mojglobus34@gmail.com>
2025-07-30 09:13:46 -0400

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,12 +1,1 @@
-const animatedElements = document.getElementsByClassName('wobble-text');
-// commit moment
-[...animatedElements].forEach(elm => {
-    let text = elm.innerText.split('');
-    elm.innerText = ''; // Clear the text in the div
 
-    text.forEach(letter => {
-        const span = document.createElement('span');
-        span.innerText = letter;
-        elm.appendChild(span);
-    });
-});
